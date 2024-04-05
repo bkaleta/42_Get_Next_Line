3b642114e6417349a7533aaacdb65f56a0dfe1d2
Bartosz Kaleta <bkaleta@c3r14s3.42warsaw.pl>
2024-04-05 13:06:30 +0200

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,21 @@
 /*   By: bkaleta <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:49:33 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/04/05 10:49:38 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/04/05 11:06:39 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+//get_next_line_utils.c
+int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+
+//get_next_line.c
+char	*get_next_line(int fd);
+
+#endif
