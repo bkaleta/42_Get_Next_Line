ce09203b1b1149c57e924bfe6b40b717e78de0c3
Bartosz Kaleta <bkaleta@c3r15s1.42warsaw.pl>
2024-04-23 15:51:52 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,28 @@
 /*   By: bkaleta <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:49:30 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/04/23 13:56:42 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/04/23 15:51:11 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buff;
+	static char	*hold;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (0);
+	line = ft_readline(fd, buff, hold);
+	free(buff);
+	buff = NULL;
+	if (!line)
+		return (NULL);
+	hold = ft_trim(line);
+	return (line);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,108 @@
 /*   By: bkaleta <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:49:35 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/04/23 13:56:57 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/04/23 15:43:57 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int		ft_strlen(const char *str);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *str)
+{
+	int		i;
+	int		j;
+	char	*dup;
+
+	i = 0;
+	j = ft_strlen(str);
+	dup = (char *)malloc(sizeof(*dup) * (j + 1));
+	if (!dup)
+		return (NULL);
+	while (i < j)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int		i;
+	int		j;
+	char	*s3;
+
+	i = 0;
+	j = 0;
+	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!s3)
+		return (NULL);
+	while (s1[i])
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		s3[i + j] = s2[j];
+		j++;
+	}
+	s3[i + j] = '\0';
+	return (s3);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	int		i;
+	int		j;
+	char	*sub;
+
+	sub = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			sub[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	sub[j] = '\0';
+	return (sub);
+}
