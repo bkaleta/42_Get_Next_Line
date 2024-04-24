385b012ca5dcb46f5c65ad46d0341de0a4d4c1a8
Bartosz Kaleta <bkaleta@c3r14s3.42warsaw.pl>
2024-04-24 12:13:46 +0200

    update

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: bkaleta <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:49:35 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/04/23 15:43:57 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/04/24 12:12:26 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ int	ft_strlen(const char *str)
 	int	i;
 
 	i = 0;
-	if (!str)
-		return (NULL);
-	while (str[i] != '\0')
+	while (str[i])
 		i++;
 	return (i);
 }
@@ -33,15 +31,13 @@ int	ft_strlen(const char *str)
 char	*ft_strdup(const char *str)
 {
 	int		i;
-	int		j;
 	char	*dup;
 
 	i = 0;
-	j = ft_strlen(str);
-	dup = (char *)malloc(sizeof(*dup) * (j + 1));
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
 	if (!dup)
 		return (NULL);
-	while (i < j)
+	while (str[i])
 	{
 		dup[i] = str[i];
 		i++;
@@ -56,11 +52,11 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	int		j;
 	char	*s3;
 
-	i = 0;
-	j = 0;
 	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!s3)
 		return (NULL);
+	i = 0;
+	j = 0;
 	while (s1[i])
 	{
 		s3[i] = s1[i];
@@ -83,31 +79,16 @@ char	*ft_strchr(const char *s, int c)
 			return ((char *)s);
 		s++;
 	}
-	if (*s == '\0')
+	if (c == '\0')
 		return ((char *)s);
 	return (0);
 }
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
+	char	*rest_str;
 	int		i;
 	int		j;
-	char	*sub;
 
-	sub = (char *)malloc(sizeof(*s) * (len + 1));
-	if (!sub)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (i >= start && j < len)
-		{
-			sub[j] = s[i];
-			j++;
-		}
-		i++;
-	}
-	sub[j] = '\0';
-	return (sub);
+	rest_str = (char *)malloc(sizeof(char) * (len + 1));
 }
