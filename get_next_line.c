aedffa89b2230d3fa6f97fe14664a0d6f0b44fc4
Bartosz Kaleta <bkaleta@c3r14s2.42warsaw.pl>
2024-04-24 13:44:10 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,73 @@
 /*   By: bkaleta <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:49:30 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/04/24 11:24:44 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/04/24 13:43:53 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd);
+static char	*ft_readline(int fd, char *buffor, char *rest)
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	while (i != '\0')
+	{
+		i = read(fd, buffor, BUFFER_SIZE);
+		if (i == -1)
+			return (0);
+		else if (i == 0)
+			break ;
+		buffor[i] = '\0';
+		if (!rest)
+			rest = ft_strdup("");
+		tmp = rest;
+		rest = ft_strjoin(tmp, buffor);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buffor, '\n'))
+			break ;
+	}
+	return (rest);
+}
+
+static char	*ft_trim(char *line)
+{
+	size_t	count;
+	char	*rest;
+
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (line[count] == '\0' || line[1] == '\0')
+		return (0);
+	rest = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (*rest == '\0')
+	{
+		free(rest);
+		rest = NULL;
+	}
+	line[count + 1] = '\0';
+	return (rest);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buffor;
+	static char	*rest;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buffor = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffor)
+		return (0);
+	line = ft_readline(fd, buffor, rest);
+	free(buffor);
+	buffor = NULL;
+	if (!line)
+		return (NULL);
+	rest = ft_trim(line);
+	return (line);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: bkaleta <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 10:49:35 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/04/24 12:12:26 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/04/24 12:56:31 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,13 @@ int	ft_strlen(const char *str)
 
 char	*ft_strdup(const char *str)
 {
-	int		i;
 	char	*dup;
+	int		i;
 
-	i = 0;
 	dup = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
 	if (!dup)
 		return (NULL);
+	i = 0;
 	while (str[i])
 	{
 		dup[i] = str[i];
@@ -48,15 +48,13 @@ char	*ft_strdup(const char *str)
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
+	char	*s3;
 	int		i;
 	int		j;
-	char	*s3;
 
 	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!s3)
 		return (NULL);
-	i = 0;
-	j = 0;
 	while (s1[i])
 	{
 		s3[i] = s1[i];
@@ -86,9 +84,24 @@ char	*ft_strchr(const char *s, int c)
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	char	*rest_str;
+	char	*rest;
 	int		i;
 	int		j;
 
-	rest_str = (char *)malloc(sizeof(char) * (len + 1));
+	rest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!rest)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			rest[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	rest[j] = '\0';
+	return (rest);
 }
